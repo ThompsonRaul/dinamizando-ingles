9dca5a1249837363fc99e67535c564245fea7b6b
Thompson Raul <89136846+ThompsonRaul@users.noreply.github.com>
2025-03-17 18:18:33 +0000

    Criação da página `2025.1` para o `UESC English` e `Conversation Club`.

--- src/pages/uesc-english/2024-2/index.js
+++ src/pages/uesc-english/2024-2/index.js
@@ -4,19 +4,19 @@ import DinamizandoNavbar from "@/components/Navbar/DinamizandoNavbar";
 import DinamizandoAccordion from "@/components/Accordion/DinamizandoAccordion";
 import DinamizandoFooter from "@/components/Footer/DinamizandoFooter";
 
-import imgURL from "../../utils/images/25-1/UECT.png";
+import imgURL from "../../../utils/images/24-2/UECT.png";
 
-const imgAlt = "Horários de aula do UESC English 2025.1";
+const imgAlt = "Horários de aula do UESC English 2024.2";
 const jsonFirstAccordion = require("./fac.json");
 const jsonMap = require("./ue.json");
 const UE = () => {
   return (
     <>
       <Head>
-        <title>UESC English 2025.1</title>
+        <title>UESC English 2024.2</title>
         <meta
           name="description"
-          content="Página do UESC English 2025.1 do Dinamizando"
+          content="Página do UESC English 2024.2 do Dinamizando"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
